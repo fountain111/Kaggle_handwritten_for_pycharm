45801dccf8f849bb1a2fef6744ad8947ea0ef1cf
kakurong <fountainoy@iclou.com>
2016-09-23 20:06:34 +0800

    ok

--- Hand_written.py
+++ Hand_written.py
@@ -1,16 +1,22 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
+
 VALIDATION_SIZE = 2000
 FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_integer('regular', '1',
+                            """if use regularization to prevent overfittingm. 0 = not use""")
 LEARNING_RATE = 1e-4
+
+
+
 
 def split_data():
     datas = pd.read_csv('train.csv')
     labels_flat = datas[[0]].values.ravel()
     labels_count = np.unique(labels_flat).shape[0]
     images = datas.iloc[:, 1:].values.astype(np.float32)
-    labels = one_hot(labels_flat,labels_count)
+    labels = one_hot(labels_flat, labels_count)
 
     # split data into train&corss_validation
     train_images = images[VALIDATION_SIZE:]
@@ -18,17 +24,36 @@ def split_data():
     validation_images = images[:VALIDATION_SIZE]
     validation_labels = labels[:VALIDATION_SIZE]
 
-    return train_images,train_labels,validation_images,validation_labels
+    return train_images, train_labels, validation_images, validation_labels
 
 
 def inference(datas):
-    weight_1 = weight_variable([784, 10])
-    bias_1 = bias_variable([10])
-    labels = tf.nn.sigmoid(tf.matmul(datas, weight_1) + bias_1)
-    return labels
+    number_nn_1 = 500
+    w1 = weight_variable([784, number_nn_1])
+    b1 = bias_variable([number_nn_1])
 
-def loss(model_labels,labels):
-    return tf.reduce_mean(tf.square(model_labels - labels))
+    hidden1 = tf.nn.sigmoid(tf.matmul(datas, w1) + b1)
+
+    w2 = weight_variable([number_nn_1, 10])
+    b2 = bias_variable([10])
+
+    labels = tf.nn.softmax(tf.matmul(hidden1, w2) + b2)
+    if (FLAGS.regular):regularizer = regular(w2,b2)
+    else:
+        regularizer = 0
+    return labels,regularizer
+
+
+def regular(weights,biases):
+    return tf.nn.l2_loss(weights) + tf.nn.l2_loss(biases)
+
+
+def loss(model_labels, labels,regularizers):
+    loss = tf.reduce_sum(tf.square(model_labels - labels))
+    if (regularizers != 0):
+        loss += 5e-4 *  regularizers
+    return loss
+
 
 def train(loss):
     train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(loss)
@@ -47,6 +72,7 @@ def one_hot(labels_dense, num_classes):
     labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
     return labels_one_hot
 
+
 def feature_scaling(datas):
     return datas.iloc[:, 1:].values.astype(np.float)
 
@@ -56,6 +82,31 @@ def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
+
 def bias_variable(shape):
     initial = tf.constant(0.1, shape=shape)
     return tf.Variable(initial)
+
+
+def get_batch(batch_size, train_images, train_labels, index_in_epoch, epochs_completed, num_examples):
+    start = index_in_epoch
+    index_in_epoch += batch_size
+    # when all trainig data have been already used, it is reorder randomly
+    if index_in_epoch > num_examples:
+        # finished epoch
+        epochs_completed += 1
+        # shuffle the data
+        perm = np.arange(num_examples)
+        np.random.shuffle(perm)
+        train_images = train_images[perm]
+        train_labels = train_labels[perm]
+        # start next epoch
+        start = 0
+        index_in_epoch = batch_size
+        assert batch_size <= num_examples
+    end = index_in_epoch
+    return train_images[start:end], train_labels[start:end], index_in_epoch, epochs_completed
+
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
--- Hand_written_train.py
+++ Hand_written_train.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 import Hand_written
-
+BATCH_SIZE = 100
 FLAGS = tf.app.flags.FLAGS
 
-tf.app.flags.DEFINE_integer('max_steps', 10000,
+tf.app.flags.DEFINE_integer('max_steps', 5000,
                             """Number of batches to run.""")
 tf.app.flags.DEFINE_string('summary_dir', '/tmp/minist',
                            """Directory where to write event logs """
@@ -13,34 +13,50 @@ tf.app.flags.DEFINE_string('summary_dir', '/tmp/minist',
 
 def train():
     train_datas, train_labels, validation_datas, validation_labels = Hand_written.split_data()
+    epochs_completed = 0
+    index_in_epoch = 0
+    num_examples = train_datas.shape[0]
     with tf.Graph().as_default():
-        global_step = tf.Variable(0)
+        x = tf.placeholder('float', shape=[None, 784])
+        y_ = tf.placeholder('float', shape=[None, 10])
 
+        model_labels,regularizers = Hand_written.inference(x)
 
-        model_labels = Hand_written.inference(train_datas)
-
-        loss = Hand_written.loss(model_labels,train_labels)
+        loss = Hand_written.loss(model_labels,y_,regularizers)
 
         optimizer = Hand_written.train(loss)
 
+        # evaluation
+        correct_prediction = tf.equal(tf.argmax(model_labels, 1), tf.argmax(y_, 1))
+
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, 'float32'))
+
         tf.scalar_summary("loss",loss)
+        tf.scalar_summary("accuracy",accuracy)
+
         merge = tf.merge_all_summaries()
-        train_writer = tf.train.SummaryWriter(FLAGS.summary_dir)
+        train_writer = tf.train.SummaryWriter(FLAGS.summary_dir + '/train')
+        validation_writer  = tf.train.SummaryWriter(FLAGS.summary_dir + '/validation')
 
         init = tf.initialize_all_variables()
         sess = tf.Session()
         sess.run(init)
+
     for step in range(FLAGS.max_steps):
-        _,loss_value = sess.run([optimizer,feed_dict={x: batch_xs, y_: batch_ys])
+        batch_x, batch_y, index_in_epoch, epochs_completed = Hand_written.get_batch(BATCH_SIZE, train_datas, train_labels, index_in_epoch, epochs_completed,num_examples)
+        sess.run([optimizer], feed_dict={x: batch_x, y_: batch_y})
+        if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
+            validation_writer.add_summary(sess.run(merge, feed_dict={x:validation_datas[0:BATCH_SIZE], y_: validation_labels[0:BATCH_SIZE]}), step)
+            train_writer.add_summary(sess.run(merge,feed_dict={x: batch_x, y_: batch_y}), step)
+        if step ==  (FLAGS.max_steps - 1):
+            print ("done")
 
-        #if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-        print("step=", step, loss_value)
 
 
 
 
 
-def main(argv=None):  # pylint: disable=unused-argument
+def main(argv=None):
     train()
 
 
